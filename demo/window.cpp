87e6ec46e5b810f3f9617cdbe3ecb26af992ad5c
OnceGzsJAY <guozisheng2022@126.com>
2025-10-18 14:10:46 +0800

    Code : Window Component Add Main Loop's Running Function Demo

--- demo/window.cpp
+++ demo/window.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
 #include "../include/window.h"
+
+void game();
 
 int main()
 {
     tomato::window Window;
-    Window.run(800, 600, "Potato Engine");
+    Window.run(800, 600, "Potato Engine", game);
+}
+
+void game()
+{
+    std::cerr << "Potato Engine is running!" << std::endl;
 }
